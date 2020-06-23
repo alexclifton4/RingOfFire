387eb59bca15d6447e9df78400b8951772dcabd1
Alex Clifton <20780073+alexclifton4@users.noreply.github.com>
2020-06-23 16:19:42 +0100

    add player list to host controls

--- public/host.js
+++ public/host.js
@@ -6,8 +6,31 @@ window.startGame = function() {
     socket.emit("start");
 };
 
-// Reset buttn
+// Reset button
 window.stopGame = function() {
     // Tell the server
     socket.emit("stop")
 };
+
+// Receive list of users
+socket.on("users", users => {
+    // update names list
+    let userListText = "";
+    Object.keys(users).forEach(user => {
+        userListText += users[user].name + "<br>";
+    });
+    
+    // if there are none, say none
+    if (userListText == "") {
+        userListText = "<i>none</i>"
+    }
+    
+    document.getElementById("userList").innerHTML = userListText;
+});
+
+// on load, get list of users
+let begin = function() {
+  socket.emit("getUsers")
+}
+
+window.onload = begin
